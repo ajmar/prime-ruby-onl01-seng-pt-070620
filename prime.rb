49d3307deabd79a6a2e165266763499dcc44796b
Alexander Marusin <amarusin18@gmail.com>
2020-07-17 20:48:41 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,8 +1,11 @@
 def prime?(num)
-  n = 0
-  while n < num
-    return false if num % n == 0 && num < 0
+  n = 2
+ if num < 0
+   return false
+ elsif n < num
+    return false if num % n == 0
     n += 1
   end
   true
 end
+ 
